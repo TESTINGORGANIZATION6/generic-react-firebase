b39657d1b6351261c69283bd8afe3e54cb30c4a3
swapnilrabse <61693277+swapnilrabse@users.noreply.github.com>
2020-04-05 22:49:13 +0530

    view changes
    view change

--- src/components/Layout/Cards.js
+++ src/components/Layout/Cards.js
@@ -3,57 +3,65 @@ import { Container, Row, Col, Card } from 'react-bootstrap'
 
 const Cards = () => {
   return (
-    <Container className='cardContainer'>
+    <Container className="cardContainer">
       <Row>
         <Col>
-          <Card  className="cardview" style={{ width: '18rem', margin : '0 auto'}}>
+          <Card
+            className="cardview"
+            style={{ width: '18rem', margin: '0 auto' }}
+          >
             <Card.Body className="box">
-              <div className='icon iconred'>
-                01
-              </div>
-              <div className="content"> 
-              <Card.Title className="p-2">Web Development</Card.Title>
-              <Card.Subtitle className="mb-2 p-1">
-                Develop beautiful website
-              </Card.Subtitle>
-              <Card.Text>
-                We build as you like. There are three responses to a piece of
-                design – yes, no, and WOW! Wow is the one to aim for.
-              </Card.Text>
+              <div className="icon iconred">01</div>
+              <div className="content">
+                <Card.Title className="p-2">Web Development</Card.Title>
+                <Card.Subtitle className="mb-2 p-1">
+                  Develop beautiful website
+                </Card.Subtitle>
+                <Card.Text>
+                  We build as you like. There are three responses to a piece of
+                  design – yes, no, and WOW! Wow is the one to aim for.
+                </Card.Text>
               </div>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card  className="cardview"  style={{ width: '18rem', margin : '0 auto'}}>
+          <Card
+            className="cardview"
+            style={{ width: '18rem', margin: '0 auto' }}
+          >
             <Card.Body className="box">
-            <div className='icon icongreen'>
-                02
-              </div>
-              <div className="content"> 
-              <Card.Title className="p-2">Software Development</Card.Title>
-              <Card.Subtitle className="mb-2 p-1">Software you enjoy</Card.Subtitle>
-              <Card.Text>
-                Make things as simple as possible, but not simpler. Software and
-                cathedrals are much the same – first we build them, then we pray
-              </Card.Text>
+              <div className="icon icongreen">02</div>
+              <div className="content">
+                <Card.Title className="p-2">Software Development</Card.Title>
+                <Card.Subtitle className="mb-2 p-1">
+                  Software you enjoy
+                </Card.Subtitle>
+                <Card.Text>
+                  Make things as simple as possible, but not simpler. Software
+                  and cathedrals are much the same – first we build them, then
+                  we pray
+                </Card.Text>
               </div>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card  className="cardview"  style={{ width: '18rem', margin : '0 auto'}}>
+          <Card
+            className="cardview"
+            style={{ width: '18rem', margin: '0 auto' }}
+          >
             <Card.Body className="box">
-            <div className='icon iconblue'>
-                03
-              </div>
-              <div className="content"> 
-              <Card.Title className="p-2">Crazy Enthusiasm</Card.Title>
-              <Card.Subtitle className="mb-2 p-1">Grow each day</Card.Subtitle>
-              <Card.Text>
-                Technology over technique produces emotionless design.The only
-                strategy that is guaranteed to fail is not taking risks.
-              </Card.Text>
+              <div className="icon iconblue">03</div>
+              <div className="content">
+                <Card.Title className="p-2">Crazy Enthusiasm</Card.Title>
+                <Card.Subtitle className="mb-2 p-1">
+                  Grow each day
+                </Card.Subtitle>
+                <Card.Text>
+                  Technology over technique produces emotionless design.The only
+                  strategy that is guaranteed to fail is not taking risks.
+                </Card.Text>
               </div>
             </Card.Body>
           </Card>
--- src/components/Layout/ContactUs.js
+++ src/components/Layout/ContactUs.js
@@ -3,24 +3,30 @@ import { Form, Row, Col, Button } from 'react-bootstrap'
 
 const ContactUs = () => {
   return (
-    <div className='contactBox'>
+    <div className="contactBox">
       <Row className="justify-content-md-center p-3 m-0">
         {/* <Col xs className="mailimg" lg="6">
         </Col> */}
         <Col xs lg="5">
           <h2 className="text-center title">Contact Us</h2>
           <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Name" />        
+            <Form.Group controlId="formBasicEmail" className="inputbox">
+              <Form.Control type="text" required="required" />
+              <Form.Label className="text">Name</Form.Label>
+              <span className="line"></span>
             </Form.Group>
-            <Form.Group>
-            <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Email" />
+            <Form.Group className="inputbox">
+              <Form.Control type="email" required="required" />
+              <Form.Label className="text">Email</Form.Label>
+              <span className="line"></span>
             </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Your Query</Form.Label>
-              <Form.Control as="textarea" rows="3" />
+            <Form.Group
+              controlId="exampleForm.ControlTextarea1"
+              className="inputbox textarea"
+            >
+              <Form.Control as="textarea" rows="3" required="required" />
+              <Form.Label className="text">Your Query</Form.Label>
+              <span className="line"></span>
             </Form.Group>
             <Button className="submitBtn" type="submit">
               Submit
@@ -28,7 +34,7 @@ const ContactUs = () => {
           </Form>
         </Col>
       </Row>
-      </div>
+    </div>
   )
 }
 
