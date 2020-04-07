71c3cf53a279736191dfa75890d0d83dd2d4c7fe
pprachit09 <pprachit09@gmail.com>
2020-04-07 01:48:49 -0300

    Added site URL

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API = process.env.REACT_APP_API_URL
+// Need a way to define this securely. possible solution is to use cloud functions
+export const API = 'https://salty-citadel-56178.herokuapp.com/api/v1'
